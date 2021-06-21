f0cad64e3d870a04bb084c206838486d03e57593
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-06-21 19:41:39 +0500

    Currency Converter

---
+++ Currency Converter/app.js
@@ -0,0 +1,21 @@
+function convert() {
+    let rupees = parseInt(document.querySelector('input').value);
+    if (document.querySelector('#unit').value == "D") {
+
+        let dollars = Math.round(rupees / 156.53);
+        console.log(dollars)
+        document.querySelector('span').innerHTML = "The amount in Dollar is " + dollars;
+    }
+    else if (document.querySelector('#unit').value == "R") {
+        let riyals = Math.round(rupees / 41.83);
+
+        document.querySelector('span').innerHTML = "The amount in Saudi-Riyal is " + riyals;
+    }
+    else if (document.querySelector('#unit').value == "P") {
+        let pounds = Math.round(rupees / 216.63);
+        document.querySelector('span').innerHTML = "The amount in Pounds is " + pounds;
+    }
+
+}
+
+
